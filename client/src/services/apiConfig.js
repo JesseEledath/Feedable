ac162a415af369f8308a382b2f142d20178ff5d3
rehack65 <dojojesse@gmail.com>
2021-03-08 16:27:31 -0500

    fillied in placholder info for APIconfig

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 let apiUrl
 
 const apiUrls = {
-    production: 'placeholder',
+    production: 'https://feedable-app.herokuapp.com/api',
     development: 'http://localhost:3000/api'
 }
 
