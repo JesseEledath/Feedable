a1057821e5983c3d324b97300c91743554e6f593
rehack65 <dojojesse@gmail.com>
2021-03-08 15:42:22 -0500

    added basic routes for backend

--- routes/projects.js
+++ routes/projects.js
@@ -1,3 +1,10 @@
 const { Router } = require('express')
 const controllers = require('../controllers/products')
 const router = Router()
+router.get('/', controllers.getProducts)
+router.get('/:id', controllers.getProduct)
+router.post('/', controllers.createProduct)
+router.put('/:id', controllers.updateProduct)
+router.delete('/:id', controllers.deleteProduct)
+
+module.exports = router
