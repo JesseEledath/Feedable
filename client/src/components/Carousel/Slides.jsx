58f590c699c868601c8727d746486ded32056114
rehack65 <dojojesse@gmail.com>
2021-03-09 15:18:19 -0500

    more carousel work

--- client/src/components/Carousel/Slides.jsx
+++ client/src/components/Carousel/Slides.jsx
@@ -1,8 +1,8 @@
 import { Carousel } from 'react-responsive-carousel'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import React from 'react';
-
 function Slides(props) {
+
   return (
     <div className = 'slides-container'>
       <Carousel infiniteLoop useKeyboardArrows autoPlay >
--- client/src/screens/LandingPage/LandingPage.jsx
+++ client/src/screens/LandingPage/LandingPage.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import Slides from '../../components/Carousel/Slides'
-import Filter from '../../components/Filter/Filter'
+import React, { useEffect, useState } from "react";
+import Slides from "../../components/Carousel/Slides";
+import Filter from "../../components/Filter/Filter";
+import { getProducts } from "../../services/crud";
+
 function LandingPage(props) {
+  const [allProducts, setAllProducts] = useState([]);
+  // Useeffect for AllProducts ========================
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const products = await getProducts();
+      setAllProducts(products);
+    };
+    fetchProducts();
+  }, []);
+  // Rendering =====================================
   return (
-    <div className='landing-page-container'>
-      <Slides />
-      <Filter />
+    <div className="landing-page-container">
+      <Slides allProducts={allProducts} />
+      <Filter allProducts={allProducts} />
     </div>
   );
 }
