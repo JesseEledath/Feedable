e1281a60eda6a71b607149d65bb0697f37ebf3c9
Bryan Molk <brybrym92@MacBook-Pro-3.local>
2021-03-10 10:47:54 -0500

    search not working

---
+++ client/src/components/Search/Search.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Search.css'
+
+const Search = (props) => {
+  return (
+    <form className="search-form" onSubmit={(e) => props.onSubmit(e)}>
+      <input
+        className="search-input"
+        type="text"
+        value={props.value}
+        onChange={(e) => props.onChange(e)}
+        name="Search"
+        placeholder="Search"
+        autoFocus
+      />
+      <button type="submit">Submit</button>
+      </form>
+  )
+}
+
+export default Search
--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,15 +1,57 @@
 import './Home.css'
+import React, { useState, useEffect } from 'react'
 import Products from '../../components/Products/Products'
-// import Search from '../../components/Search/Search'
+import Search from '../../components/Search/Search'
+import Product from '../../components/Product/Product.jsx'
+import Sort from '../../components/Sort/Sort'
+import { getProducts } from '../../services/crud'
+import { AZ, ZA} from "../../utils/sort"
 // import Layout from '../../components/shared/Layout/Layout'
 
 const Home = () => {
+  const [allProducts, setAllProducts] = useState([])
+  const [queriedProducts, setQueriedProducts] = useState([])
+  const [sortType, setSortType] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const products = await getProducts()
+      setAllProducts(products)
+      setQueriedProducts(products)
+    }
+    fetchProducts()
+  }, [])
+
+  const handleSort = type => {
+    setSortType(type)
+    switch (type) {
+      case "name-ascending":
+        setQueriedProducts(AZ(queriedProducts))
+        break
+      case "name-descending":
+        setQueriedProducts(ZA(queriedProducts))
+        break
+      default:
+        break
+    }
+  }
+  const handleSearch = event => {
+    const newQueriedProducts = allProducts.filter(product => product.name.toLowerCase().includes(event.target.value.toLowerCase()))
+    setQueriedProducts(newQueriedProducts, () => handleSort(sortType))
+  }
+
+  const handleSubmit = event => event.preventDefault()
+
+  const productsJSX = queriedProducts.map((product, index) =>
+    <Product _id={product._id} name={product.name} imgURL={product.imgURL} key={index} />
+  )
   return (
     <div>
       {/* <Layout user={props.user}> */}
-        {/* <Search onSubmit={handleSubmit} onChange={handleSearch} /> */}
+      <Search onSubmit={handleSubmit} onChange={handleSearch} />
+      {/* <Sort onSubmit={handleSubmit} onChange={handleSort} /> */}
         <div className="products">
-          <Products />
+        {productsJSX}
         </div>
       {/* </Layout> */}
     </div>
