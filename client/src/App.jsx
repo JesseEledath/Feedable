d3f8f73a0547eccfb0e8ef472339c594573761c8
Bryan Molk <brybrym92@MacBook-Pro-3.local>
2021-03-11 15:44:19 -0500

    merge

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,74 +5,66 @@ import Create from "./screens/Create/Create";
 import Home from "./screens/Home/Home";
 import LandingPage from "./screens/LandingPage/LandingPage";
 import ProductDetail from "./screens/ProductDetail/ProductDetail";
-import ProductEdit from "./screens/ProductEdit/ProductEdit"
+import ProductEdit from "./screens/ProductEdit/ProductEdit";
 import SignIn from "./screens/SignIn/SignIn";
 import SignOut from "./screens/SignOut/SignOut";
 import SignUp from "./screens/SignUp/SignUp";
-import About from "./screens/About/About"
-import {verifyUser} from './services/users'
+import About from "./screens/About/About";
+import { verifyUser } from "./services/users";
 
-import './App.css';
+import "./App.css";
 
 const App = () => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     const fetchUser = async () => {
-      const user = await verifyUser()
-      user ? setUser(user) : setUser(null)
-    }
-    fetchUser()
-  }, [])
+      const user = await verifyUser();
+      user ? setUser(user) : setUser(null);
+    };
+    fetchUser();
+  }, []);
 
-  const clearUser = () => setUser(null)
+  const clearUser = () => setUser(null);
   // console.log("App", user)
   return (
     <div className="App">
       <Switch>
         <Route exact path="/">
-          <LandingPage user={user}/>
+          <LandingPage user={user} />
         </Route>
         <Route path="/sign-up">
-
-          <SignUp setUser={setUser}/>
-
+          <SignUp setUser={setUser} />
         </Route>
         <Route path="/sign-in">
-          <SignIn setUser={setUser}/>
+          <SignIn setUser={setUser} />
         </Route>
         <Route path="/sign-out">
-
-          <SignOut setUser={setUser} clearUser={clearUser}/>
-
+          <SignOut setUser={setUser} clearUser={clearUser} />
         </Route>
         <Route exact path="/products">
-          <Home user={user}/>
+          <Home user={user} />
         </Route>
         <Route exact path="/products/:id">
-          <ProductDetail user={user}/>
+          <ProductDetail user={user} />
         </Route>
 
         <Route exact path="/products/:id/edit">
-          <ProductEdit user={user}/>
+          <ProductEdit user={user} />
         </Route>
 
         <Route exact path="/about">
           <About />
         </Route>
-        
+
         <Route exact path="/cart">
           {/* {user ? <Cart user={user} /> : <Redirect to="/sign-up" />} */}
           <Cart />
         </Route>
 
-
-
         <Route exact path="/create">
-
           <Create />
         </Route>
-
       </Switch>
     </div>
   );
