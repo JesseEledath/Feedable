24997a840a14dd01c45ec2ad09919ce63c5ef0a8
Bilal Khundmiri <khundmirib@gmail.com>
2021-03-11 11:09:45 -0500

    signin

--- client/src/screens/SignIn/SignIn.jsx
+++ client/src/screens/SignIn/SignIn.jsx
@@ -73,11 +73,7 @@ const SignIn = (props) => {
           type="text"
           name="email"
           value={email}
-<<<<<<< HEAD
-          placeholder="Enter Email"
-=======
           placeholder="Email"
->>>>>>> 6e978515d6244cb73905ae92623dd4589a2de064
           onChange={handleChange}
         />
         <input
