3dda94ecb3d77467e451fdf8fcf38757220fec64
Bilal Khundmiri <khundmirib@gmail.com>
2021-03-11 12:05:47 -0500

    working on cart

--- client/src/screens/Cart/Cart.jsx
+++ client/src/screens/Cart/Cart.jsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
 import Layout from "../../components/shared/Layout/Layout"
+import { getProducts } from "../../services/crud";
 
 export default function Cart (props) {
-    
+    const [products, setProducts] = useState([])
 
-    console.log(localStorage);
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const res = await getProducts()
+            setProducts(res)
+        }
+        fetchProducts()
+        console.log(products);
+    }, [])
+
+    const listProducts = products.map(product => (
+        <div key={product._id}></div>
+    ))
+    // console.log(localStorage);
 
     return (
         <Layout>
