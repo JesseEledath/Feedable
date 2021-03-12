35e35a55c3ddbf1206d0316038e549e2dfad8644
cbscar <cbscar@gmail.com>
2021-03-12 15:02:54 -0600

    in-progress on Filter

---
+++ client/src/components/Checkbox/Checkbox.jsx
@@ -0,0 +1,19 @@
+import "./Checkbox.css";
+
+const Checkbox = (props) => {
+  // console.log(props)
+  return (
+    <>
+      <input
+        key={props.index}
+        // onClick={props.handleCheckChildElement}
+        onChange={(e) => props.onChange(e)}
+        type="checkbox"
+        // checked={props.isChecked}
+        value={props.item.name} />
+      {props.item.name}
+    </>
+  );
+};
+
+export default Checkbox;
