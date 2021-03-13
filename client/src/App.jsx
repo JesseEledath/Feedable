65eccafcecc8af10a76537924d3ec44d36ca2694
Bilal Khundmiri <khundmirib@gmail.com>
2021-03-13 15:13:21 -0500

    loading before rendering app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,6 +17,7 @@ import AllUsers from "./screens/AllUsers/AllUsers";
 
 const App = () => {
   const [user, setUser] = useState(null);
+  const [toggleLoad, setToggleLoad] =useState(false)
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -30,8 +31,18 @@ const App = () => {
 
   // console.log(user.role);
 
+  useEffect(() => {
+    setTimeout((toggleLoad) => {
+      setToggleLoad(true)
+    }, 500);
+  })
+
+  
+
   return (
     <div className="App">
+      {!toggleLoad ? <div>Loading...</div> 
+      : 
       <Switch>
         <Route exact path="/">
           <LandingPage user={user} />
@@ -79,6 +90,7 @@ const App = () => {
           {user?.role === "Admin" ? <Create user={user}/> : <Redirect to="/products"/> }
         </Route>
       </Switch>
+      }
     </div>
   );
 };
